da3e466efd6b925c55975d99217d840a11b4c353
NimsTay <ntayefeh@vols.utk.edu>
2022-01-05 21:55:21 -0500

    fixed php

--- contact.php
+++ contact.php
@@ -1,5 +1,12 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
 if(isset($_POST['Submit']))
 {
     $name = $_POST['Name'];
@@ -7,10 +14,32 @@ if(isset($_POST['Submit']))
     $subject = $_POST['Subject'];
     $message = $_POST['Message'];
 
-    $mailfrom = "nimstayphotos@yahoo.com";
     $mailto = "nimstayphotos@yahoo.com";
     $headers = "From:".$email;
     $txt = "You have received a message from ".$name.".\n\n".$message;
-    
-    mail($mailto, $subject, $txt, $headers);
+ 
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->Mailer = "smtp";
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;               // Enable SMTP authentication 
+    $mail->Username = 'nimstayp@gmail.com';   // SMTP username 
+    $mail->Password = 'nimstaypHOST22!';   // SMTP password 
+    $mail->SMTPSecure = 'tls';            // Enable TLS encryption, `ssl` also accepted 
+    $mail->Port = 587;
+
+    $mail->setFrom("nimstayphotos@yahoo.com", "NimsTay Photos");
+    $mail->addAddress($email, $name);
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->Body = $txt;
+    if(!$mail->Send()) 
+    {
+        echo "Error while sending Email.";
+        var_dump($mail);
+    } 
+    else 
+    {
+        echo "Email sent successfully";
+    }
 }
